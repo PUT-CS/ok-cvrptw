89ccbb4546fbf9cb186777561decbab05bf1b8b4
Michał Miłek <michal.mileq@gmail.com>
2022-10-21 18:39:48 +0200

    Refactor, modify README.md

--- main.cpp
+++ main.cpp
@@ -9,21 +9,14 @@ using std::string;
 
 class Depot {
 public:
-    int num;
-    int x;
-    int y;
-    int demand;
-    int ready_time;
-    int end_time;
-    int service_duration;
+    int num, x, y, demand, ready_time, end_time, service_duration;
     Depot() {}
     ~Depot() {}
 };
 
 class Problem {
 public:
-    int num_of_trucks;
-    int truck_capacity;
+    int num_of_trucks, truck_capacity;
     std::vector<Depot> depots;
     
     void readFrom(std::string filename) {
@@ -36,15 +29,33 @@ public:
 
 	while (!input.eof()) {
 	    Depot new_depot;
-	    input>>new_depot.num>>new_depot.x>>new_depot.y>>new_depot.demand>>new_depot.ready_time>>new_depot.end_time>>new_depot.service_duration;
+	    input>>
+		new_depot.num>>
+		new_depot.x>>
+		new_depot.y>>
+		new_depot.demand>>
+		new_depot.ready_time>>
+		new_depot.end_time>>
+		new_depot.service_duration;
 	    this->depots.push_back(new_depot);
 	}
     }
     void print() {
-	std::cout<<"Truck Number: "<<this->num_of_trucks<<std::endl<<"Truck Capacity: "<<this->truck_capacity<<std::endl;
+	std::cout<<"Truck Number: "
+		 <<this->num_of_trucks<<std::endl
+		 <<"Truck Capacity: "
+		 <<this->truck_capacity<<std::endl;
 	for (unsigned long int i=0; i<this->depots.size(); i++) {
 	    Depot d = this->depots[i];
-	    std::cout<<d.num<<'\t'<<d.x<<'\t'<<d.y<<'\t'<<d.demand<<'\t'<<d.ready_time<<'\t'<<d.end_time<<'\t'<<d.service_duration<<'\t'<<std::endl;
+	    std::cout
+		<<d.num<<'\t'
+		<<d.x<<'\t'
+		<<d.y<<'\t'
+		<<d.demand<<'\t'
+		<<d.ready_time<<'\t'
+		<<d.end_time<<'\t'
+		<<d.service_duration<<'\t'
+		<<std::endl;
 	}
     }
 };
